a40a7030ffad6146a4df7d1b9361fc7da14395ac
Aluhug <94222690+Aluhug@users.noreply.github.com>
2024-06-14 14:06:24 +0300

    initialize

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+SECRET_KEY = '6342eb430febed6def4d4063a2fa717907fb4911143de1891c59dca9e0519bd8'
+
+MYSQL_USER = 'std_2478_kursovaya'
+MYSQL_DATABASE = 'std_2478_kursovaya'
+MYSQL_PASSWORD = '123456789'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
